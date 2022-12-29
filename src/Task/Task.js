6c078b61074df12551467420ea09df1134ad446d
Paul <pasha.khimich@mail.ru>
2022-12-29 14:54:31 +0300

    "new repo"

---
+++ src/Task/Task.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Task({value}) {
+    return (
+        <div className='view'>
+            <input className='toggle' type="checkbox"/>
+            <label>
+                <span className='description'>{value}</span>
+            </label>
+            <button className='icon icon-edit'></button>
+            <button className='icon icon-destroy'></button>
+        </div>
+    );
+}
---
+++ src/TaskList/TaskList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function TaskList() {
+    return (
+        <section>
+            <ul className='taskList'>
+                
+            </ul>
+        </section>
+    );
+}
+
